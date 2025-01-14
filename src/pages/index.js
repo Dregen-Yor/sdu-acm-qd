bf9bd0afec5a95e92215fff3a9c7b0269cfe647c
Dregen_Yor <hgcatqdao@outlook.com>
2025-01-14 23:47:17 +0800

    fix error

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
@@ -40,7 +39,6 @@ export default function Home() {
         <HomepageFeatures />
       </main>
       <Analytics />
-      <SpeedInsights />
     </Layout>
   );
 }
